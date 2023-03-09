b88935b0ac32ff8da5b2f7b5578c0c9141684fe7
JPavelka97 <jacob.pavelka.13@gmail.com>
2023-03-09 11:16:19 -0600

    commit

---
+++ React/box_generator/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import ColorForm from './components/Form'
+import Boxes from './components/Boxes'
+
+function App() {
+
+
+  return (
+    <div>
+      <ColorForm></ColorForm>
+      <Boxes></Boxes>
+    </div>
+  );
+}
+
+export default App;
---
+++ React/box_generator/src/components/Boxes.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const Boxes = props => {
+    const [color, setColor] = useState("");
+    const [width, setWidth] = useState("");
+    const [height, setHeight] = useState("")
+  
+    const generateBox = (color,width,height) => {
+      setColor(color)
+      setWidth(width)
+      setHeight(height)
+    }
+    
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Boxes
